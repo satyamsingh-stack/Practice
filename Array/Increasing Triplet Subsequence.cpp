88daa42adefb1eb8c2c6d7b851a9980e03c0e315
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-09-01 23:10:49 +0530

    Create Increasing Triplet Subsequence.cpp

---
+++ Array/Increasing Triplet Subsequence.cpp
@@ -0,0 +1,20 @@
+//Time Comp: O(N)
+//Space Comp: O(1)
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        if(nums.size()<3)
+            return false;
+        int left=INT_MAX;
+        int mid=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>mid)    return true;
+            else if(nums[i]>left && nums[i]<mid)
+                mid=nums[i];
+            else if(nums[i]<left)
+                left=nums[i];
+        }
+        return false;
+    }
+};
