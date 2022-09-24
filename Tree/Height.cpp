feb5ae93e1ad7121e6214eadc8b7f58af8b2fae5
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-09-24 10:08:06 +0530

    Create Height.cpp

---
+++ Tree/Height.cpp
@@ -0,0 +1,15 @@
+//Time Comp: O(N)
+//Space Comp: O(1)
+
+
+class Solution{
+    public:
+    //Function to find the height of a binary tree.
+    int height(struct Node* node){
+        if(node==NULL)
+            return 0;
+        int lh=height(node->left);
+        int rh=height(node->right);
+        return max(lh,rh)+1;
+    }
+};
